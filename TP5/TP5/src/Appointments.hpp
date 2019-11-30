57a221c60fa6d7706432614b5cc2c623200766d9
Nathan Ramsay-Vejlens <nathan.ramsay-vejlens@polymtl.ca>
2019-11-30 15:52:31 -0500

    Ajout fichier et restructuration de classe Appointments et AppointmentsData

---
+++ TP5/TP5/src/Appointments.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <numeric>
+
+#include "Constants.hpp"
+#include "CSVHandler.hpp"
+
+class Appointments
+{
+public:
+    Appointments(std::string filePath);
+    void display(unsigned int userCol, std::string userId, std::vector<unsigned int> displayedColumns = {});
+    bool schedule(std::string patientId, std::string nurseId);
+
+
+    std::vector<std::vector<std::string>>* getData();
+    void setData(std::vector<std::vector<std::string>> data);
+private:
+    std::vector<std::string> createNewAppointment(std::string patientId, std::string nurseId);
+
+    std::vector<std::vector<std::string>> _appointmentsData;
+    std::vector<std::string> _headers;
+    CSVHandler _csvHandler;
+
+    const unsigned int _COLUMN_WIDTH = 14;
+};
